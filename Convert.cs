123b0f4103d1c0e81050a56319520d11143f4df5
mathe <mathe@DESKTOP-06LM2TJ>
2024-08-29 16:46:17 -0300

    Adicionar arquivos de projeto.

---
+++ Convert.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula4_cs_ex3
+{
+    public class Convert
+    {
+        public List<double> Logica1(List<double> ListaD)
+        {
+            if (ListaD[0] >= ListaD[1] && ListaD[0] >= ListaD[2])
+            {
+                return ListaD;
+            }
+            else if (ListaD[1] >= ListaD[0] && ListaD[1] >= ListaD[2])
+            {
+                double aux;
+                aux = ListaD[0];
+                ListaD[0] = ListaD[1];
+                ListaD[1] = aux;
+            }
+            else
+            {
+                double aux1, aux2;
+                aux1 = ListaD[0];
+                aux2 = ListaD[1];
+                ListaD[0] = ListaD[2];
+                ListaD[1] = aux1;
+                ListaD[2] = aux2;
+            }
+
+            return ListaD;
+        }
+        public int Logica2(List<double> ListaD)
+        {
+            if (ListaD[0] * ListaD[0] == ListaD[1] * ListaD[1] + ListaD[2] * ListaD[2])
+            {
+                return 0;
+            }
+            else if (ListaD[0] * ListaD[0] > ListaD[1] * ListaD[1] + ListaD[2] * ListaD[2])
+            {
+                return 1;
+            }
+            else if (ListaD[0] * ListaD[0] < ListaD[1] * ListaD[1] + ListaD[2] * ListaD[2])
+            {
+                return 2;
+            }
+            return 3;
+
+        }
+        public double Logica3(List<double> ListaD)
+        {
+            if (ListaD[0] < ListaD[1] + ListaD[2] || ListaD[1] < ListaD[0] + ListaD[2] || ListaD[2] < ListaD[0] + ListaD[1])
+            {
+                double resultado1 = (ListaD[0] + ListaD[1] + ListaD[2]) / 2.0;
+                double area = Math.Sqrt(resultado1 * (resultado1 - ListaD[0]) * (resultado1 - ListaD[1]) * (resultado1 - ListaD[2]));
+                return area;
+            }
+            else
+            {
+                Console.WriteLine("Erro o valor não é um triangulo");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula4_cs_ex3
+{
+
+    /*Na mesma classe do exercício 03, crie um método que receba 3 valores, lados de
+    um triângulo. O seu método deve verificar se os valores recebidos formam um
+    triângulo.
+    Se formarem um triângulo, calcular e escrever a área do triângulo formado e
+    retornar este valor.
+    Se não formarem um triângulo o seu método deve retornar o valor zero.
+    Na classe Program testar também este novo método.*/
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+        public Program()
+        {
+            Convert Converter = new Convert();
+            List<string> listaS = new List<string>();
+            List<double> listaD = new List<double>();
+            List<string> listaS2 = new List<string>();
+            List<double> listaD2 = new List<double>();
+            int retorno;
+
+            Console.WriteLine("Digite os 3 valores: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite o valor {0}/3: ", i + 1);
+                listaS.Add(Console.ReadLine());
+            }
+            listaD = listaS.ConvertAll(double.Parse);
+            Converter.Logica1(listaD);
+            retorno = Converter.Logica2(listaD);
+            if (retorno == 0)
+            {
+                Console.WriteLine("0 - Triângulo Retângulo");
+            }
+            else if (retorno == 1)
+            {
+                Console.WriteLine("1 - Triângulo Acutângulo");
+            }
+            else if (retorno == 2)
+            {
+                Console.WriteLine("2 - Triângulo Obtusângulo");
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("Exercicio 4");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite o valor {0}/3: ", i + 1);
+                listaS2.Add(Console.ReadLine());
+            }
+            listaD2 = listaS2.ConvertAll(double.Parse);
+            double area = Converter.Logica3(listaD2);
+            Console.WriteLine("A área do triângulo é: {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
